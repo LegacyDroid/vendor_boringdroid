4e444ea87eba5be3f486754ac72f167bb66da7d8
minhmc2007 <quangminh21072010@gmail.com>
2026-06-26 09:59:56 +0700

    boringdroid.mk: build BoringdroidSystemUI from source
    - Add BoringdroidSystemUI to PRODUCT_PACKAGES
    - Add BoringdroidSystemUI path to PRODUCT_SOONG_NAMESPACES
    
    Change-Id: I46af541502099010c2236e39b106f5f3b68d18c4

--- boringdroid.mk
+++ boringdroid.mk
@@ -1,17 +1,14 @@
-# Recents
+# LegacyDex / BoringDroid integration
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.recents.grid=true \
     persist.sys.pcmode.enabled=true \
     persist.sys.systemuiplugin.enabled=true \
 
-PRODUCT_PACKAGES := \
-    BoringdroidSystemUIApk \
-    BoringdroidSettingsApk \
-
-# rro overlay
 PRODUCT_PACKAGES += \
-    BoringdroidSystemUIOverlay
+    BoringdroidSystemUIOverlay \
+    BoringdroidSystemUI
 
-PRODUCT_COPY_FILES := \
-    frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/boringdroid \
+    vendor/packages/apps/BoringdroidSystemUI
 
